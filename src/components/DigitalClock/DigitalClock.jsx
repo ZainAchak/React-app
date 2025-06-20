45ef7e269210f99246a698c16ec82860ac754ce5
Zain Ullah <zainullahachakzai@gmail.com>
2025-06-20 16:21:25 +0500

    clock new feature

--- src/components/DigitalClock/DigitalClock.jsx
+++ src/components/DigitalClock/DigitalClock.jsx
@@ -1,11 +1,34 @@
 import React, {useState, useEffect} from "react"
 import styles from "./DigitalClock.module.css"
 
+function formatTime(date) {
+        let hours = date.getHours();
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12 || 12; // Convert 0 (midnight) to 12
+        const formattedHours = String(hours).padStart(2, '0');
+
+        return `${formattedHours}:${minutes}:${seconds} ${ampm}`;
+        
+    }
+
 function DigitalClock() {
+    const [time, setTime] = useState(formatTime(new Date()));
+
+    useEffect(()=>{
+        const intervalId = setInterval(() => {
+            setTime(formatTime(new Date()));
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    },[]);
+
     return(
         <div className={styles.clockContainer}>
             <div className={styles.clock}>
-                <span>00:00:00:00:00</span>
+                <span>{time}</span>
             </div>
         </div>
     )
